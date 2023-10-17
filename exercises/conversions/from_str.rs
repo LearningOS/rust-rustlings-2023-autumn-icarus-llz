dbd1bc475efbbf26609eb3b8ce70664bfa783ccd
yangkeqiang <1604050047@qq.com>
2023-10-18 00:27:44 +0800

    100

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,7 +51,25 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let tmp:Vec<_> = s.split(",").collect();
+        if s.len() == 0 {return{Err(ParsePersonError::Empty)};}
+        else if tmp.len() != 2 {return{Err(ParsePersonError::BadLen)};}
+        else if  tmp[0] == ""  {return {Err(ParsePersonError::NoName)};}
+        let age = tmp[1].parse();
+        match age {
+            Ok(age) =>Ok(
+            Person{
+                name:tmp[0].to_string(),
+                age:age,
+            }
+                ),
+            Err(e) => Err(ParsePersonError::ParseInt(e)),
+        }
+
+        
+
     }
+
 }
 
 fn main() {
